99363f38aac967dd04f2875f524e04ec4376f40e
Krunal <krunal@Krunals-MacBook-Air.local>
2025-02-10 20:10:03 +0530

    new changes with fixes

--- app/api/send/route.tsx
+++ app/api/send/route.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 // import { EmailTemplate } from '../../../components/EmailTemplate';
 import type { NextApiRequest, NextApiResponse } from 'next';
 
